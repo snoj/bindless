ad8466dec79de39dd30d1daf82daea9ecda53325
Josh Erickson <josh@snoj.us>
2015-02-12 00:49:47 -0600

    Some movement forward
    add: skeleton for watching updates.
    add: bl.observable
    
    Signed-off-by: Josh Erickson <josh@snoj.us>

--- showoff.js
+++ showoff.js
@@ -5,3 +5,12 @@ bli.redraw();
 var bli2 = new bl();
 bli2.set('inception', {innard: "space"})
 bli2.redraw();
+
+
+var a = bl.observable({test: 1, c: {a:2}});
+
+console.log(a());
+
+var b = bl.observable({test: 1, c: {a:2}}, {recurse: true});
+
+console.log(b());
