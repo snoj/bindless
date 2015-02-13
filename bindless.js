84940d8deac86c63965aa1bcef8b745c0f0573b2
Josh Erickson <josh@snoj.us>
2015-02-12 23:14:51 -0600

    Rewrote bl.observable
    bug: updates are one behind and reset the form input to the previous
      entry on blur.
    
    Signed-off-by: Josh Erickson <josh@snoj.us>

--- bindless.js
+++ bindless.js
@@ -24,6 +24,45 @@
 
       var o = _.clone(opts);
       self.root = o.root || $('body');
+      self.on('redraw', self.redraw);
+      self.blroot = bl.observable({});
+      self.blroot.on('mutated change', function() {
+        self.trigger('redraw');
+      })
+    }
+    ,set: function(key, val, options) {
+      var self = this;
+      var attrs;
+      if (key == null) return this;
+
+      // Handle both `"key", value` and `{key: value}` -style arguments.
+      if (typeof key === 'object') {
+        attrs = key;
+        options = val;
+      } else {
+        (attrs = {})[key] = val;
+      }
+
+      options || (options = {});
+
+      _.each(attrs, function(v, k) {
+        if (!!self.attributes[k] && !!!v.__bl_proto__) {
+          self.attributes[k](v);
+          delete attrs[k];
+        }
+        else if(!!!v.__bl_proto__) {
+          attrs[k] = bl.observable(v, {recurse: options.recurse}, self.blroot);
+          attrs[k].on('mutated change', function() { self.trigger('redraw'); });
+        } else if (!!v.__bl_proto__) {
+          v.parent = self.blroot;
+          attrs[k] = v;
+          attrs[k].on('mutated change', function() { self.trigger('redraw'); });
+        }
+      });
+
+      var r = Backbone.Model.prototype.set.call(this, attrs, options);
+      self.redraw();
+      return r;
     }
     ,redraw: function(options) {
       var self = this;
@@ -38,12 +77,70 @@
         });
     }
   });
+  bl.observableProperties = {
+    __bl_proto__: true
+  };
+  bl.observable = function(value, options, parent) {
+    options || (options = {});
+
+    var _oldvalue = value;
+    var ratelimit = options.ratelimit || 1;
+    var ratecounter = 0;
+
+    var ob = function(newValue, ioptions) {
+      ioptions || (ioptions = {})
+      var self = arguments.callee;
+      if(arguments.length == 0)
+        return _oldvalue;
+      
+      if(arguments.length == 1) {
+        if(!_.isEqual(_oldvalue, newValue)) {
+
+          self.trigger('prechange', newValue, _oldvalue, ob);
+
+          if(!!!(ratecounter % ratelimit)) {
+            ratecounter = 1;
+            if(!!!ioptions.silent) {
+              self.trigger('change', newValue, _oldvalue, ob);
+              if(self.parent) self.parent.trigger('mutated', ob, newValue, _oldvalue);
+            }
+          } else {
+            ratecounter++;
+          }
+          _oldvalue = newValue;
+        }
+        //return self;
+      }
+    };
+    _.extend(ob, _.clone(bl.observableProperties));
+    _.extend(ob, {
+      options: options
+      ,parent: parent
+      ,toJSON: function() { return _oldvalue; }
+      //,toString: function() { return _oldvalue; }
+    });
+    _.extend(ob, Backbone.Events);
+    ob.on('mutated', function(originator, newValue, _oldvalue) {
+      //redraw
+      //notify parents
+      if (ob.parent) ob.parent.trigger('mutated', originator, newValue, _oldvalue);
+    });
+
+    if(options.recurse) {
+      _.each(value, function(v, k) {
+        value[k] = bl.observable(value[k], options, ob);
+      });
+    }
+
+    return ob;
+  }
+
   var extractnproc = function(root, context, domo, text) {
     if(typeof context.$root === 'undefined')
       Object.defineProperty(context, '$root', {value: root}); //self.attributes?
 
     //todo shorten this long line if possible
-    var raw = (new Function('$context', '$domo', 'try { with($context) { var ro = { ' + text + ' }; $domo.data("blcontext", $context); return ro; } } catch (ex) { $domo.removeData("blcontext"); return ex; }'))(context, domo);
+    var raw = (new Function('$context', '$domo', 'try { with($context) { var ro = { ' + text + ' }; $domo.data("blcontext", $context); return ro; } } catch (ex) { $domo.removeData("blcontext"); console.error(ex); return ex; }'))(context, domo);
     //if raw instanceof Error it is bad code or non-existent object tree for this context.
     if(raw instanceof Error) return;
 
@@ -55,7 +152,21 @@
   };
   bl.handlers = {
     value: function(domo, context, arg) {
-      domo.val(arg);
+      var v = (typeof arg === 'function') ? arg() : arg;
+      domo.val(v);
+      if (!!!domo.data('blevent-value') && !!arg.__bl_proto__) {
+        var changeHandler = function(e) {
+          arg(domo.val());
+        };
+        domo.data('blevent-value', changeHandler);
+        domo.on('change', changeHandler);
+      }
+    }
+    ,text: function(domo, context, arg) {
+      domo.text(arg);
+    }
+    ,html: function(domo, context, arg) {
+      domo.html(arg);
     }
   };
   
--- showoff.js
+++ showoff.js
@@ -1,16 +1,19 @@
 var bli = new bl();
-bli.set('meohmy', 1234)
-bli.redraw();
+bli.on('change redraw', function() {
+  $('#test1 .watch pre').text(JSON.stringify(bli, null, "  "));
+});
+bli.set('meohmy', 1234);
 
 var bli2 = new bl();
 bli2.set('inception', {innard: "space"})
 bli2.redraw();
 
+try {
+  var a = bl.observable({test: 1, c: {a:2}});
 
-var a = bl.observable({test: 1, c: {a:2}});
+  console.log(a());
 
-console.log(a());
+  var b = bl.observable({test: 1, c: {a:2}}, {recurse: true});
 
-var b = bl.observable({test: 1, c: {a:2}}, {recurse: true});
-
-console.log(b());
+  console.log(b());
+} catch(ex) {}
